0e5972a8a3fc027d99a69fe27f20cf99b9dbc3c9
koushik00-tech <koushik.koppolu00@gmail.com>
2025-02-11 18:54:57 +0530

    Updated Code

---
+++ ajakus-app/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+
+import { ToastContainer } from 'react-toastify'; // Import ToastContainer
+import 'react-toastify/dist/ReactToastify.css'; // Import default styles for notifications
+import UserList from './components/UserList';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <h1>User Management Dashboard</h1>
+      </header>
+      <main>
+        <UserList />
+      </main>
+      <ToastContainer position="top-right" autoClose={3000} /> {/* Toast container for top-right notifications */}
+    </div>
+  );
+}
+
+export default App;
